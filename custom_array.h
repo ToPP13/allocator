58081dbc5a6c74c174e77ba9b40467f7b75a1394
Kirill <Kirill>
2020-11-07 16:55:58 +0300

    Revert "remove all files for review”"
    This reverts commit 6a266cd407a26ea0802e1020bcfaf2502bc09b7e.

---
+++ custom_array.h
@@ -0,0 +1,53 @@
+#ifndef CUSTOM_ARRAY_H
+#define CUSTOM_ARRAY_H
+
+#include <memory>
+#include <stdexcept>
+
+
+template<typename T>
+void print_array(T array, size_t array_size)
+{
+    for (size_t i = 0; i < array_size; ++i)
+        std::cout << i << " " << array.get(i) << std::endl;
+}
+
+
+template<typename T, size_t N, typename ALLOCATOR = std::allocator<T>>
+class CustomArray
+{
+public:
+    CustomArray(): mem_pointer(nullptr)
+    {
+        auto ptr = allocator_.allocate(N);
+        mem_pointer = std::shared_ptr<T>(ptr);
+        for (size_t i=0; i < N; i++)
+            allocator_.construct(mem_pointer.get() + i, T());
+
+    }
+    ~CustomArray()
+    {
+        for (size_t i=0; i < N; i++)
+        {
+            allocator_.destroy(mem_pointer.get() + i);
+        }
+//        allocator_.deallocate(mem_pointer.get(), N);
+    }
+    bool set_elem(std::size_t n, const T &t)
+    {
+        if (n >= N)
+            throw std::out_of_range{""};
+        allocator_.construct(mem_pointer.get() + n, t);
+        return true;
+    }
+
+    T get(std::size_t n)
+    {
+        return *(mem_pointer.get() + n);
+    }
+private:
+    std::shared_ptr<T> mem_pointer;
+    ALLOCATOR allocator_;
+};
+
+#endif // CUSTOM_ARRAY_H
